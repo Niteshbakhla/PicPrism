458a07e2375539797b38488d95a2386a0e35552f
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-06-25 22:07:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
+import { SellerDashboard } from './pages/SellerDashboard'
+import { BuyerDashboard } from './pages/BuyerDashboard'
+import { Navbar } from './components/Navbar'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/seller' element={<SellerDashboard />} />
+          <Route path='/buyer' element={<BuyerDashboard />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+
---
+++ src/pages/Main.jsx
@@ -0,0 +1,4 @@
+import { Home } from "./Home";
+import { Signup } from "./Signup";
+
+

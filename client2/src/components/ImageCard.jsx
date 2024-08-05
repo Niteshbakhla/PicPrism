6563c5ca83b847dcb093513ffbb5b999b2696ee4
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-05 22:48:52 +0530

    created a image card component.

--- client2/src/components/ImageCard.jsx
+++ client2/src/components/ImageCard.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 
-const ImageCard = () => {
+export function ImageCard({ id, title, image, author, price }) {
   return (
-    <div>ImageCard</div>
-  )
+    <Card className="mt-6 w-96" id={id}>
+      <CardHeader color="blue-gray" className="relative h-56">
+        <img
+          src={image}
+          alt="card-image"
+          className="w-full h-full object-cover"
+        />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {title}
+        </Typography>
+        <Typography color="gray" className="mb-2">
+          Author: {author}
+        </Typography>
+        <Typography variant="h6" color="green">
+          ${price}
+        </Typography>
+        <Typography className="mt-4">
+          The place is close to Barceloneta Beach and bus stop just 2 min by
+          walk and near to &quot;Naviglio&quot; where you can enjoy the main
+          night life in Barcelona.
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button>Read More</Button>
+      </CardFooter>
+    </Card>
+  );
 }
-
-export default ImageCard

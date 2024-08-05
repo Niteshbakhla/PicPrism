ee1325f8c28aba6d90f8c6bb94eafff409df0b8a
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-05 22:51:31 +0530

    #

--- client2/src/pages/Home.jsx
+++ client2/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 export const Home = () => {
 
+            const addPost = (e) => {
+                        e.preventDefault()
+                    console.log(e.target.name)
+            }
+
             return (
                         <>
-                        home</>
+                                    <form onSubmit={addPost}>
+                                                <input type="text" name="title" placeholder="write something here" />
+                                                <input type="number" name="num" placeholder="Type number here" />
+                                                <button type="submit">Submit</button>
+                                    </form>
+                        </>
             )
 }

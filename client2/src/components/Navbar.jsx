78893a1a11d5759fdde8e5ee61801fdb2acca6a3
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-07 17:51:26 +0530

    added a axios library

--- client2/src/components/Navbar.jsx
+++ client2/src/components/Navbar.jsx
@@ -10,7 +10,9 @@ import {
 } from "@material-tailwind/react";
 import toast, { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { logout } from "../store/slice/authSlice";
+import { login, logout } from "../store/slice/authSlice";
+import axios from "axios";
+
 
 export function Nav() {
             const [openNav, setOpenNav] = React.useState(false);
@@ -59,6 +61,29 @@ export function Nav() {
                         }, 1000);
             }
 
+
+            const refreshToken = async () => {
+                        try {
+                                    const res = await axios.get(import.meta.env.VITE_API_URL + "/refresh", {
+                                                headers: {
+                                                            "Authorization": "Bearer " + localStorage.getItem("refreshToken")
+                                                }
+                                    })
+
+                                    const data = await res.data;
+                                    dispatch((login(data)))
+                        } catch (error) {
+                                    dispatch(logout())
+                        }
+            }
+
+
+            useEffect(() => {
+                        const interval = setInterval(() => {
+                                    refreshToken();
+                        }, 1000 * 60 * 13)
+            })
+
             const navList = (
                         <ul className="  mt-2  mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
                                     <Typography
@@ -81,7 +106,7 @@ export function Nav() {
                                                 className="  font-medium"
                                     >
 
-                                                <Link to="/seller/profile" className={`flex items-center text-[20px] transition-all hover:bg-black hover:text-white px-4 lg:rounded-full  ${location.pathname === "/contact" && "bg-black text-white"}`}>
+                                                <Link to="/seller/profile" className={`flex items-center  ${location.pathname === "/seller/profile" || location.pathname === "/buyer/profile" ? "block" : "hidden"} text-[20px] transition-all hover:bg-black hover:text-white px-4 lg:rounded-full  ${location.pathname === "/contact" && "bg-black text-white"}`}>
                                                             Profile
                                                 </Link>
                                     </Typography>
@@ -178,43 +203,43 @@ export function Nav() {
                                                                         </div>
                                                             </div>
                                                 </div>
-                                                            <IconButton
-                                                                        variant="text"
-                                                                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                                                                        ripple={false}
-                                                                        onClick={() => setOpenNav(!openNav)}
-                                                            >
-                                                                        {openNav ? (
-                                                                                    <svg
-                                                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                                                fill="none"
-                                                                                                className="h-6 w-6"
-                                                                                                viewBox="0 0 24 24"
-                                                                                                stroke="currentColor"
-                                                                                                strokeWidth={2}
-                                                                                    >
-                                                                                                <path
-                                                                                                            strokeLinecap="round"
-                                                                                                            strokeLinejoin="round"
-                                                                                                            d="M6 18L18 6M6 6l12 12"
-                                                                                                />
-                                                                                    </svg>
-                                                                        ) : (
-                                                                                    <svg
-                                                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                                                className="h-6 w-6"
-                                                                                                fill="none"
-                                                                                                stroke="currentColor"
-                                                                                                strokeWidth={2}
-                                                                                    >
-                                                                                                <path
-                                                                                                            strokeLinecap="round"
-                                                                                                            strokeLinejoin="round"
-                                                                                                            d="M4 6h16M4 12h16M4 18h16"
-                                                                                                />
-                                                                                    </svg>
-                                                                        )}
-                                                            </IconButton>
+                                                <IconButton
+                                                            variant="text"
+                                                            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                                                            ripple={false}
+                                                            onClick={() => setOpenNav(!openNav)}
+                                                >
+                                                            {openNav ? (
+                                                                        <svg
+                                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                                    fill="none"
+                                                                                    className="h-6 w-6"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    stroke="currentColor"
+                                                                                    strokeWidth={2}
+                                                                        >
+                                                                                    <path
+                                                                                                strokeLinecap="round"
+                                                                                                strokeLinejoin="round"
+                                                                                                d="M6 18L18 6M6 6l12 12"
+                                                                                    />
+                                                                        </svg>
+                                                            ) : (
+                                                                        <svg
+                                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                                    className="h-6 w-6"
+                                                                                    fill="none"
+                                                                                    stroke="currentColor"
+                                                                                    strokeWidth={2}
+                                                                        >
+                                                                                    <path
+                                                                                                strokeLinecap="round"
+                                                                                                strokeLinejoin="round"
+                                                                                                d="M4 6h16M4 12h16M4 18h16"
+                                                                                    />
+                                                                        </svg>
+                                                            )}
+                                                </IconButton>
                                     </div>
                                     <Collapse open={openNav}>
                                                 <div className="container mx-auto">

0044aa9a6357afa6bedef2d1cd96469c515defa2
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-07 17:51:09 +0530

    accecpt two icons one is edit and second is delete icon from react icons

--- client2/src/components/ImageCard.jsx
+++ client2/src/components/ImageCard.jsx
@@ -7,9 +7,9 @@ import {
   Button,
 } from "@material-tailwind/react";
 
-export function ImageCard({ id, title, image, author, price }) {
+export function ImageCard({ id, title, image, author, price, icon1, icon2 }) {
   return (
-    <Card className="mt-12 w-96 " id={id}>
+    <Card className="mt-12 w-100 m-2 " id={id}>
       <CardHeader color="blue-gray" className="relative ">
         <img
           src={image}
@@ -21,15 +21,21 @@ export function ImageCard({ id, title, image, author, price }) {
         <Typography variant="h5" color="blue-gray" className="mb-2">
           {title}
         </Typography>
-        <Typography color="gray" className="mb-2">
-          Author: {author}
-        </Typography>
+
         <Typography variant="h6" color="green">
           ${price}
         </Typography>
       </CardBody>
       <CardFooter className="pt-0">
-        <Button>Read More</Button>
+        <div className="flex">
+          {
+            icon1
+          }
+
+          {
+            icon2
+          }
+        </div>
       </CardFooter>
     </Card>
   );

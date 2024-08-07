f2f37742af95d878ff86f0a84e93a5e0727d3cf3
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-07 17:48:48 +0530

    added a protected route

--- client2/src/App.jsx
+++ client2/src/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate, Navigate } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Home } from './pages/Home'
 import { Signup } from './pages/Signup'
 import { SellerDashboard } from './pages/SellerDashboard'
 import { BuyerDashboard } from './pages/BuyerDashboard'
 import { Nav } from './components/Navbar'
+import { ProtectedRoute } from './components/Protectedroutes'
 
 
 function App() {
   const navigate = useNavigate()
+
   useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
       navigate("/login")
@@ -19,12 +21,12 @@ function App() {
   return (
     <>
       <Nav />
-      <Routes>
+      <Routes location={location}>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/seller/profile' element={<SellerDashboard />} />
-        <Route path='/buyer/profile' element={<BuyerDashboard />} />
+        <Route path='/login' element={<ProtectedRoute children={<Login />} requiresAuth={false} />} />
+        <Route path='/signup' element={<ProtectedRoute children={<Signup />} requiresAuth={false} />} />
+        <Route path='/seller/profile' element={<ProtectedRoute children={<SellerDashboard />} />} />
+        <Route path='/buyer/profile' element={<ProtectedRoute children={<BuyerDashboard />} />} />
       </Routes>
     </>
   )

fd4fdfc76c010800bbcd6bb2dfe5473835850144
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-08-12 16:50:42 +0530

    added verifyToken for refresh token

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
-const { login, signup } = require("../controllers/authControllers")
+const { login, signup, refresh } = require("../controllers/authControllers")
+const { verifyToken } = require("../middlewares/verifyToken")
 
 const router = require("express").Router()
 
 router.post("/login", login)
 router.post("/signup", signup)
+router.get("/refresh", verifyToken, refresh)
 
 module.exports = router 
